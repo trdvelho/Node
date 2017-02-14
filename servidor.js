d0468079388b5cfb8db212a0fd22f80aef978885
trdvelho <trdvelho@yahoo.com.br>
2017-02-14 17:40:58 -0200

    Real-time NodeJs Chat

---
+++ servidor.js
@@ -0,0 +1,20 @@
+// server.js
+ // É necessário instanciar os módulos abaixo nessa ordem!
+ var app = require('express')()
+   , server = require('http').createServer(app)
+   , io = require('socket.io').listen(server);
+ server.listen(3000, function(){
+   console.log("Chat real-time...");
+ });
+ app.get('/', function (req, res) {
+   res.sendFile(__dirname + '/index.html');
+ });
+ // Eventos do Socket.IO
+ io.sockets.on('connection', function (socket) {
+   socket.on('toServer', function (data) {
+   	 var br = "<br>";
+     var msg = br+data.nome+":"+data.msg;
+     socket.emit('toClient', msg);
+     socket.broadcast.emit('toClient', msg);
+   });
+ });
